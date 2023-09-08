b734e3b7b939ec2c5df2631b035ed80d73b9467b
DzmitryievaYuliyaFront <superjulija@gmail.com>
2023-09-08 14:44:51 +0300

    inkrement/dekrement

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,4 @@
-let number = 1;
-let number1 = number;
-let number3 = ++number + --number;
+let number = 1, number1 = number, number3 = ++number + --number
 console.log(number1);
 console.log(number3);
 console.log(number3);
